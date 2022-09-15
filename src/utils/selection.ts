cb079db91bfe9863e7614df97b1ceff72bc3fe5a
Максим Травинцев <smsochneg@yandex-team.ru>
2022-09-15 16:35:15 +0300

    fix: instanceof issue

--- src/utils/selection.ts
+++ src/utils/selection.ts
@@ -1,12 +1,35 @@
+import {Node, NodeType} from 'prosemirror-model';
 import {Selection, TextSelection, NodeSelection, AllSelection} from 'prosemirror-state';
 
 export const isTextSelection = (selection: Selection): selection is TextSelection =>
-    selection instanceof TextSelection;
+    // for some reason "instanceof" sometimes returns false
+    selection.constructor.name === TextSelection.prototype.constructor.name;
 
 export const isNodeSelection = (selection: Selection): selection is NodeSelection =>
-    selection instanceof NodeSelection;
+    // for some reason "instanceof" sometimes returns false
+    selection.constructor.name === NodeSelection.prototype.constructor.name;
 
 // ts broke down when use "selection is AllSelection" return type
 // maybe because AllSelection has same class type with different constructor
 export const isWholeSelection = (selection: Selection): boolean =>
-    selection instanceof AllSelection;
+    // for some reason "instanceof" sometimes returns false
+    selection.constructor.name === AllSelection.prototype.constructor.name;
+
+export const equalNodeType = function equalNodeType(nodeType: NodeType[] | NodeType, node: Node) {
+    return (Array.isArray(nodeType) && nodeType.indexOf(node.type) > -1) || node.type === nodeType;
+};
+
+export const findSelectedNodeOfType = (nodeType: NodeType) => {
+    return function (selection: Selection) {
+        if (isNodeSelection(selection)) {
+            const node = selection.node,
+                $from = selection.$from;
+
+            if (equalNodeType(nodeType, node)) {
+                return {node: node, pos: $from.pos, depth: $from.depth};
+            }
+        }
+
+        return null;
+    };
+};
