4ccfdaa2f5c5f6e27c89d87181f1d6c615888c9c
Максим Травинцев <smsochneg@yandex-team.ru>
2022-10-05 10:54:10 +0600

    feat: added new field for selection interface

--- src/utils/selection.ts
+++ src/utils/selection.ts
@@ -1,19 +1,20 @@
 import {Node, NodeType} from 'prosemirror-model';
 import {Selection, TextSelection, NodeSelection, AllSelection} from 'prosemirror-state';
 
+NodeSelection.prototype.selectionName = 'NodeSelection';
+TextSelection.prototype.selectionName = 'TextSelection';
+AllSelection.prototype.selectionName = 'AllSelection';
+
 export const isTextSelection = (selection: Selection): selection is TextSelection =>
-    // for some reason "instanceof" sometimes returns false
-    selection.constructor.name === TextSelection.prototype.constructor.name;
+    selection.selectionName === TextSelection.prototype.selectionName;
 
 export const isNodeSelection = (selection: Selection): selection is NodeSelection =>
-    // for some reason "instanceof" sometimes returns false
-    selection.constructor.name === NodeSelection.prototype.constructor.name;
+    selection.selectionName === NodeSelection.prototype.selectionName;
 
 // ts broke down when use "selection is AllSelection" return type
 // maybe because AllSelection has same class type with different constructor
 export const isWholeSelection = (selection: Selection): boolean =>
-    // for some reason "instanceof" sometimes returns false
-    selection.constructor.name === AllSelection.prototype.constructor.name;
+    selection.selectionName === AllSelection.prototype.selectionName;
 
 export const equalNodeType = function equalNodeType(nodeType: NodeType[] | NodeType, node: Node) {
     return (Array.isArray(nodeType) && nodeType.indexOf(node.type) > -1) || node.type === nodeType;
