dc66438151542a436409253a1f3c8b7776ff693d
Yuriy Demidov <d3m1d0v@yandex-team.ru>
2022-10-13 19:42:43 +0300

    feat(core): add context to extension builder (#22)
    The key-value storage context has been added to the ExtensionBuilder, which allows you to transfer data between extensions.

--- src/extensions/markdown/Html/index.ts
+++ src/extensions/markdown/Html/index.ts
@@ -1,3 +1,4 @@
+import {logger} from '../../../logger';
 import {createExtension, ExtensionAuto} from '../../../core';
 import {HtmlNode} from './const';
 import {fromYfm} from './fromYfm';
@@ -5,6 +6,11 @@ import {spec} from './spec';
 import {toYfm} from './toYfm';
 
 export const Html: ExtensionAuto = (builder) => {
+    if (builder.context.has('html') && builder.context.get('html') === false) {
+        logger.info('[HTML extension]: Skip extension, because HTML disabled via context');
+        return;
+    }
+
     builder.addNode(HtmlNode.Block, () => ({
         spec: spec[HtmlNode.Block],
         fromYfm: {tokenSpec: fromYfm[HtmlNode.Block]},
@@ -24,3 +30,14 @@ export const Html: ExtensionAuto = (builder) => {
  * Remove after WIKI-16660
  */
 export const HtmlE = createExtension((b, o = {}) => b.use(Html, o));
+
+declare global {
+    namespace YfmEditor {
+        interface Context {
+            /**
+             * Same as @type {MarkdownIt.Options.html}
+             */
+            html: boolean;
+        }
+    }
+}
