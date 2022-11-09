4aaeb88c6f33b30e5ec5b69701ce8635fea5c408
Yuriy Demidov <d3m1d0v@yandex-team.ru>
2022-11-09 20:33:12 +0300

    test(Code): add test to check inline code overlapping

--- src/extensions/markdown/Code/Code.test.ts
+++ src/extensions/markdown/Code/Code.test.ts
@@ -2,17 +2,22 @@ import {builders} from 'prosemirror-test-builder';
 import {createMarkupChecker} from '../../../../tests/sameMarkup';
 import {ExtensionsManager} from '../../../core';
 import {BaseNode, BaseSchema} from '../../base/BaseSchema';
+import {bold, Bold} from '../Bold';
+import {italic, Italic} from '../Italic';
 import {code, Code} from './index';
 
 const {schema, parser, serializer} = new ExtensionsManager({
-    extensions: (builder) => builder.use(BaseSchema, {}).use(Code, {}),
+    extensions: (builder) =>
+        builder.use(BaseSchema, {}).use(Bold, {}).use(Code, {}).use(Italic, {}),
 }).buildDeps();
 
-const {doc, p, c} = builders(schema, {
+const {doc, p, b, i, c} = builders(schema, {
     doc: {nodeType: BaseNode.Doc},
     p: {nodeType: BaseNode.Paragraph},
+    b: {nodeType: bold},
+    i: {nodeType: italic},
     c: {nodeType: code},
-}) as PMTestBuilderResult<'doc' | 'p', 'c'>;
+}) as PMTestBuilderResult<'doc' | 'p', 'b' | 'i' | 'c'>;
 
 const {same} = createMarkupChecker({parser, serializer});
 
@@ -21,4 +26,10 @@ describe('Code extension', () => {
 
     it('should parse code inside text', () =>
         same('he`llo wor`ld!', doc(p('he', c('llo wor'), 'ld!'))));
+
+    it('should parse and serialize overlapping inline marks', () =>
+        same(
+            'This is **strong *emphasized text with `code` in* it**',
+            doc(p('This is ', b('strong ', i('emphasized text with ', c('code'), ' in'), ' it'))),
+        ));
 });
