e46c5129547bbfb6de989d2237e14643f46cbdd0
Yuriy Demidov <d3m1d0v@yandex-team.ru>
2023-01-23 20:32:16 +0300

    fix: replace asterisk exports with named exports (#55)

--- src/extensions/behavior/Selection/index.ts
+++ src/extensions/behavior/Selection/index.ts
@@ -1,7 +1,8 @@
 import type {ExtensionAuto} from '../../../core';
 import {selection} from './selection';
+import * as SelectionCommands from './commands';
 
-export * as SelectionCommands from './commands';
+export {SelectionCommands};
 
 export const Selection: ExtensionAuto = (builder) => {
     builder.addPlugin(selection);
