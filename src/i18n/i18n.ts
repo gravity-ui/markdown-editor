4e2c49c5f4a562738724f0739b7de359e697c56e
d3m1d0v <d3m1d0v@yandex-team.ru>
2023-04-10 20:01:37 +0300

    feat: add i18n

---
+++ src/i18n/i18n.ts
@@ -0,0 +1,26 @@
+import {I18N, I18NFn} from '@gravity-ui/i18n';
+import {getConfig, Lang, subscribeConfigure} from '../configure';
+
+type KeysData = Record<string, string | string[]>; // @gravity-ui/i18n inner type
+
+const i18n = new I18N();
+
+// en – default lang for editor
+i18n.setLang(getConfig().lang || Lang.En);
+
+subscribeConfigure((config) => {
+    if (config.lang) {
+        i18n.setLang(config.lang);
+    }
+});
+
+export {i18n};
+export function registerKeyset<K extends string, D extends KeysData>(
+    keyset: K,
+    data: Record<'en' | 'ru', D>,
+) {
+    i18n.registerKeyset(Lang.En, keyset, data.en);
+    i18n.registerKeyset(Lang.Ru, keyset, data.ru);
+
+    return (i18n as unknown as I18NFn<Record<K, D>>).keyset(keyset);
+}
