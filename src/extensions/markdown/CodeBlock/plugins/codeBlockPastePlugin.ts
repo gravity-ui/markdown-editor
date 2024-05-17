b762bab4b72e7132a63412483b577db7f5058092
Sergey Makhnatkin <sergey.makhnatkin@gmail.com>
2024-05-17 11:48:42 +0200

    fix(eslint): fixed some lint errors

--- src/extensions/markdown/CodeBlock/plugins/codeBlockPastePlugin.ts
+++ src/extensions/markdown/CodeBlock/plugins/codeBlockPastePlugin.ts
@@ -1,17 +1,19 @@
-import {Plugin} from 'prosemirror-state';
-import {handlePaste} from '../handle-paste';
 import {Slice} from 'prosemirror-model';
+import {Plugin} from 'prosemirror-state';
 
-export const codeBlockPastePlugin = () => new Plugin({
-    props: {
-        handleDOMEvents: {
-            paste(view, e: Event) {
-                if (handlePaste(view, e as ClipboardEvent, Slice.empty)) {
-                    e.preventDefault();
-                    return true;
-                }
-                return false;
+import {handlePaste} from '../handle-paste';
+
+export const codeBlockPastePlugin = () =>
+    new Plugin({
+        props: {
+            handleDOMEvents: {
+                paste(view, e: Event) {
+                    if (handlePaste(view, e as ClipboardEvent, Slice.empty)) {
+                        e.preventDefault();
+                        return true;
+                    }
+                    return false;
+                },
             },
         },
-    },
-})
+    });
