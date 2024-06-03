db3749ae150686a33ee1b07ce8b5129535dca292
Yuriy Demidov <d3m1d0v@yandex-team.ru>
2024-06-03 18:38:45 +0300

    feat(bundle)!: remove deprecated YfmEditor component (#258)

--- demo/editor-in-editor/index.tsx
+++ demo/editor-in-editor/index.tsx
@@ -1,9 +1,9 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useEffect} from 'react';
 
 import {toaster} from '@gravity-ui/uikit/toaster-singleton-react-18';
 
 import {BaseNode} from '../../src';
-import {YfmEditor, YfmEditorRef} from '../../src/bundle';
+import {YfmEditorView, useYfmEditor} from '../../src/bundle';
 import {VERSION} from '../../src/version';
 import {block} from '../cn';
 
@@ -16,27 +16,31 @@ import {
 const b = block('playground');
 
 export const PlaygroundEditorInEditor: React.FC = () => {
-    const editorRef = useRef<YfmEditorRef>(null);
+    const mdEditor = useYfmEditor({
+        initialEditorType: 'wysiwyg',
+        initialToolbarVisible: true,
+        allowHTML: false,
+        linkify: true,
+        breaks: true,
+        extraExtensions: (builder) =>
+            builder.use(EditorInEditor, {
+                toaster,
+            }),
+    });
 
     useEffect(() => {
-        if (editorRef.current) {
-            const view = editorRef.current._wysiwygView;
-            if (view) {
-                const {schema} = view.state;
-                const tr = view.state.tr;
-                tr.insert(0, [
-                    schema.node(
-                        BaseNode.Paragraph,
-                        null,
-                        schema.text('This is for development only'),
-                    ),
-                    schema.node(editorInEditorNodeName, {
-                        [EditorInEditorAttr.Markup]: 'Editor-in-Editor content',
-                    }),
-                    schema.node(BaseNode.Paragraph, null, schema.text('After editor')),
-                ]);
-                view.dispatch(tr);
-            }
+        const view = mdEditor._wysiwygView;
+        if (view) {
+            const {schema} = view.state;
+            const tr = view.state.tr;
+            tr.insert(0, [
+                schema.node(BaseNode.Paragraph, null, schema.text('This is for development only')),
+                schema.node(editorInEditorNodeName, {
+                    [EditorInEditorAttr.Markup]: 'Editor-in-Editor content',
+                }),
+                schema.node(BaseNode.Paragraph, null, schema.text('After editor')),
+            ]);
+            view.dispatch(tr);
         }
     }, []);
 
@@ -48,22 +52,12 @@ export const PlaygroundEditorInEditor: React.FC = () => {
             </div>
             <hr />
             <div className={b('editor')}>
-                <YfmEditor
-                    ref={editorRef}
+                <YfmEditorView
+                    stickyToolbar
+                    settingsVisible
+                    editor={mdEditor}
                     toaster={toaster}
                     className={b('editor-view')}
-                    initialContent={''}
-                    initialEditorType={'wysiwyg'}
-                    wysiwygAllowHTML={false}
-                    wysiwygLinkify={true}
-                    wysiwygBreaks={true}
-                    settingsVisible={false}
-                    extensionOptions={{}}
-                    wysiwygExtraExtensions={(builder) =>
-                        builder.use(EditorInEditor, {
-                            toaster,
-                        })
-                    }
                 />
             </div>
         </div>
