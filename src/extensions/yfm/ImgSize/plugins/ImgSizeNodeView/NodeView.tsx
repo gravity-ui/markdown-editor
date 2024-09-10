66ca04d82f0b75752613edf5abee37fb4d9c9cd6
Sergey Makhnatkin <makhnatkin@yandex-team.ru>
2024-09-10 12:05:44 +0200

    fix(ImgSize): added ResizableImage (#338)

--- src/extensions/yfm/ImgSize/plugins/ImgSizeNodeView/NodeView.tsx
+++ src/extensions/yfm/ImgSize/plugins/ImgSizeNodeView/NodeView.tsx
@@ -1,7 +1,12 @@
-import React, {useRef} from 'react';
+import React, {useCallback, useEffect, useRef} from 'react';
 
 import {cn} from '../../../../../classname';
-import {ReactNodeViewProps} from '../../../../../react-utils/react-node-view';
+import {ReactNodeViewProps, useNodeEditing, useNodeHovered} from '../../../../../react-utils';
+import {ResizeDirection, useNodeResizing} from '../../../../../react-utils/useNodeResizing';
+import {removeNode} from '../../../../../utils';
+import {Resizable} from '../../../../behavior/Resizable/Resizable';
+import {ImgSizeAttr} from '../../ImgSizeSpecs';
+import {imageRendererKey} from '../../const';
 
 import {ImgSettingsButton} from './ImgSettingsButton';
 
@@ -15,19 +20,91 @@ export const ImageNodeView: React.FC<ReactNodeViewProps> = ({
     getPos,
     updateAttributes,
 }) => {
-    const ref = useRef<HTMLImageElement>(null);
+    const imageContainerRef = useRef<HTMLDivElement>(null);
+    const imageRef = useRef<HTMLImageElement>(null);
+
+    const alt = node.attrs[ImgSizeAttr.Alt] || '';
+    const initialHeight = node.attrs[ImgSizeAttr.Height];
+    const initialWidth = node.attrs[ImgSizeAttr.Width];
+    const src = node.attrs[ImgSizeAttr.Src] || '';
+    const title = node.attrs[ImgSizeAttr.Title] || '';
+
+    const isNodeHovered = useNodeHovered(imageContainerRef);
+    const [edit, setEditing, unsetEdit, toggleEdit] = useNodeEditing({
+        nodeRef: imageContainerRef,
+        view,
+    });
+
+    const handleResize = useCallback(
+        ({width, height}: {width?: number; height?: number}) => {
+            updateAttributes({
+                width: width === undefined ? undefined : String(Math.round(width)),
+                height: height === undefined ? undefined : String(Math.round(height)),
+                name: title,
+                alt,
+            });
+        },
+        [alt, title, updateAttributes],
+    );
+
+    const {state, startResizing} = useNodeResizing({
+        width: initialWidth,
+        height: initialHeight,
+        ref: imageRef,
+        onResize: handleResize,
+    });
+
+    const style = {
+        width: state.width ? `${state.width}px` : '',
+        height: state.height ? `${state.height}px` : '',
+        transition: 'width 0.15s ease-out, height 0.15s ease-out',
+    };
+
+    const handleDelete = useCallback(() => {
+        const pos = getPos();
+        if (pos === undefined) return;
+        removeNode({
+            node,
+            pos,
+            tr: view.state.tr,
+            dispatch: view.dispatch,
+        });
+        view.focus();
+    }, [getPos, node, view]);
+
+    const createHandleResize =
+        (direction: ResizeDirection) => (event: React.MouseEvent<HTMLElement>) => {
+            startResizing(event, direction);
+        };
+
+    useEffect(() => {
+        if (imageRendererKey.getState(view.state)?.linkAdded) {
+            setEditing();
+        }
+    }, [view, setEditing]);
 
     return (
-        <>
-            <ImgSettingsButton
-                node={node}
-                view={view}
-                getPos={getPos}
-                updateAttributes={updateAttributes}
-                nodeRef={ref}
-            />
-            {/* eslint-disable-next-line jsx-a11y/alt-text */}
-            <img {...node.attrs} ref={ref} />
-        </>
+        <div ref={imageContainerRef}>
+            <Resizable
+                hover={isNodeHovered}
+                resizing={state.resizing}
+                onResizeLeft={createHandleResize('left')}
+                onResizeRight={createHandleResize('right')}
+            >
+                <ImgSettingsButton
+                    node={node}
+                    view={view}
+                    getPos={getPos}
+                    updateAttributes={updateAttributes}
+                    visible={isNodeHovered && !edit && !state.resizing}
+                    edit={edit}
+                    toggleEdit={toggleEdit}
+                    nodeRef={imageRef}
+                    onDelete={handleDelete}
+                    unsetEdit={unsetEdit}
+                />
+                <img ref={imageRef} src={src} alt={alt} style={style} />
+            </Resizable>
+        </div>
     );
 };
