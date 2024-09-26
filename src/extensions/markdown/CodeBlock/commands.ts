24c4b24477511d43e549987858420a0b2d20ee3f
d3m1d0v <d3m1d0v@yandex-team.ru>
2024-09-26 15:43:46 +0300

    feat(CodeBlock): unification of code block creation logic

--- src/extensions/markdown/CodeBlock/commands.ts
+++ src/extensions/markdown/CodeBlock/commands.ts
@@ -1,17 +1,39 @@
+import {newlineInCode, setBlockType} from 'prosemirror-commands';
 import type {Command} from 'prosemirror-state';
 
+import type {ExtensionDeps} from '../../../core';
 import {toParagraph} from '../../base/BaseSchema';
 
-import {cbType} from './const';
+import {codeBlockType} from './const';
+
+export {newlineInCode};
 
 export const resetCodeblock: Command = (state, dispatch, view) => {
     const {selection} = state;
     if (
         selection.empty &&
-        selection.$from.parent.type === cbType(state.schema) &&
+        selection.$from.parent.type === codeBlockType(state.schema) &&
         view?.endOfTextblock('backward', state)
     ) {
         return toParagraph(state, dispatch, view);
     }
     return false;
 };
+
+export const setCodeBlockType =
+    ({serializer}: ExtensionDeps): Command =>
+    (state, dispatch) => {
+        const nodeType = codeBlockType(state.schema);
+
+        if (!setBlockType(nodeType)(state)) return false;
+
+        if (dispatch) {
+            const markup = serializer.serialize(state.selection.content().content);
+            dispatch(
+                state.tr.replaceSelectionWith(
+                    nodeType.createAndFill({}, markup ? state.schema.text(markup) : null)!,
+                ),
+            );
+        }
+        return true;
+    };
