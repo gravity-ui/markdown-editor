e470a0cbf36b769553c905c6e3af88aad4660699
d3m1d0v <d3m1d0v@yandex-team.ru>
2024-10-17 18:04:45 +0300

    refactor(extensions)!: move Mermaid extension from yfm to additional folder

--- demo/Playground.tsx
+++ demo/Playground.tsx
@@ -20,9 +20,9 @@ import {
 } from '../src';
 import type {EscapeConfig, ToolbarActionData} from '../src/bundle/Editor';
 import {Extension} from '../src/cm/state';
+import {Mermaid} from '../src/extensions/additional/Mermaid';
 import {FoldingHeading} from '../src/extensions/yfm/FoldingHeading';
 import {Math} from '../src/extensions/yfm/Math';
-import {Mermaid} from '../src/extensions/yfm/Mermaid';
 import {YfmHtmlBlock} from '../src/extensions/yfm/YfmHtmlBlock';
 import {getSanitizeYfmHtmlBlock} from '../src/extensions/yfm/YfmHtmlBlock/utils';
 import {cloneDeep} from '../src/lodash';
