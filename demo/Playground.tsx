e5f34d503aa81eee12d408394e03125a05c06a8f
d3m1d0v <d3m1d0v@yandex-team.ru>
2024-10-17 18:13:17 +0300

    refactor(extensions)!: move FoldingHeading extension from yfm to additional folder

--- demo/Playground.tsx
+++ demo/Playground.tsx
@@ -21,9 +21,9 @@ import {
 } from '../src';
 import type {ToolbarActionData} from '../src/bundle/Editor';
 import {Extension} from '../src/cm/state';
+import {FoldingHeading} from '../src/extensions/additional/FoldingHeading';
 import {Math} from '../src/extensions/additional/Math';
 import {Mermaid} from '../src/extensions/additional/Mermaid';
-import {FoldingHeading} from '../src/extensions/yfm/FoldingHeading';
 import {YfmHtmlBlock} from '../src/extensions/yfm/YfmHtmlBlock';
 import {getSanitizeYfmHtmlBlock} from '../src/extensions/yfm/YfmHtmlBlock/utils';
 import {cloneDeep} from '../src/lodash';
