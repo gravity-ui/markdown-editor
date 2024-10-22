6dbbc52e7e3e187a0ad980182881edc747d6462d
makhnatkin <makhnatkin@yandex-team.ru>
2024-10-22 14:51:39 +0200

    feat!: bump diplodoc/transform to v4.33.0 and update import of runtime styles and scripts
    - bump diplodoc/transform to v4.33.0
    - import yfm scripts and styles without yfmCut and yfmTabs runtime

--- src/extensions/yfm/YfmConfigs/index.ts
+++ src/extensions/yfm/YfmConfigs/index.ts
@@ -1,11 +1,13 @@
-import '@diplodoc/transform/dist/js/yfm';
+import '@diplodoc/transform/dist/js/base';
+import '@diplodoc/transform/dist/js/_yfm-only'; // eslint-disable-line import/order
 import {Plugin} from 'prosemirror-state';
 
 import type {ExtensionAuto} from '../../../core';
 
 import {YfmConfigsSpecs, YfmConfigsSpecsOptions} from './YfmConfigsSpecs';
 
-import '@diplodoc/transform/dist/css/yfm.css';
+import '@diplodoc/transform/dist/css/base.css';
+import '@diplodoc/transform/dist/css/_yfm-only.css'; // eslint-disable-line import/order
 import './yfm.scss'; // eslint-disable-line import/order
 
 export type YfmConfigsOptions = YfmConfigsSpecsOptions & {};
