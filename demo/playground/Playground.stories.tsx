de0302529d48e71d7ff24fabcb8cf0f1b627b7f4
dnbragin <dnbragin@yandex-team.ru>
2024-11-05 14:53:54 +0300

    chore: fix initial in Playground story

--- demo/playground/Playground.stories.tsx
+++ demo/playground/Playground.stories.tsx
@@ -1,6 +1,7 @@
 import type {Meta, StoryObj} from '@storybook/react';
 
 import {defaultArgs} from '../constants/default-args';
+import {getInitialMd} from '../utils/getInitialMd';
 
 import {Playground as PlaygroundComponent, PlaygroundProps} from './Playground';
 
@@ -8,6 +9,11 @@ const meta: Meta<PlaygroundProps> = {
     title: 'Markdown Editor',
     component: PlaygroundComponent,
     args: defaultArgs,
+    beforeEach: (context) => {
+        /* eslint-disable no-param-reassign */
+        context.args.initial = getInitialMd();
+        /* eslint-enable no-param-reassign */
+    },
 };
 
 export default meta;
