379ae817a804908d9c5d8a3da3f1f324575b7bd7
whiteformed <whiteformed@gmail.com>
2024-11-19 13:53:50 +0300

    feat(EditorView): add editor customization stories

---
+++ demo/css-variables/CSSVariables.stories.tsx
@@ -0,0 +1,33 @@
+import type {Meta, StoryObj} from '@storybook/react';
+
+import {CustomCSSVariablesDemo} from './CSSVariablesDemo';
+
+const meta: Meta = {
+    title: 'Experiments / Custom CSS Variables',
+    component: CustomCSSVariablesDemo,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof CustomCSSVariablesDemo>;
+
+export const CustomCSSVariables: Story = {
+    argTypes: {
+        '--g-md-toolbar-padding': {
+            control: {type: 'text'},
+            description: 'Toolbar padding',
+        },
+        '--g-md-toolbar-sticky-inset': {
+            control: {type: 'text'},
+            description: 'Toolbar inset in sticky mode',
+        },
+        '--g-md-toolbar-sticky-offset': {
+            control: {type: 'text'},
+            description: 'Toolbar padding in sticky mode',
+        },
+        '--g-md-editor-padding': {
+            control: {type: 'text'},
+            description: 'Editor contents padding',
+        },
+    },
+};
