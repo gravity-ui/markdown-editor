29fc648045fdb18e99c8b8e5ef64143bc8124a46
makhnatkin <makhnatkin@yandex-team.ru>
2024-11-21 09:34:04 +0100

    fix(base): disabled smart quotes

--- src/extensions/base/BaseInputRules/index.ts
+++ src/extensions/base/BaseInputRules/index.ts
@@ -1,7 +1,7 @@
-import {ellipsis, smartQuotes} from 'prosemirror-inputrules';
+import {ellipsis} from 'prosemirror-inputrules';
 
 import type {ExtensionAuto} from '../../../core';
 
 export const BaseInputRules: ExtensionAuto = (builder) => {
-    builder.addInputRules(() => ({rules: [...smartQuotes, ellipsis]}));
+    builder.addInputRules(() => ({rules: [ellipsis]}));
 };
