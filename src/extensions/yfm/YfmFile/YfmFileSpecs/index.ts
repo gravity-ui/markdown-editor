230f65a5c21c5d8b3e39519dbca3e83b971ba1b1
d3m1d0v <d3m1d0v@yandex-team.ru>
2024-11-27 20:48:03 +0300

    feat(YfmFile): support directive syntax

--- src/extensions/yfm/YfmFile/YfmFileSpecs/index.ts
+++ src/extensions/yfm/YfmFile/YfmFileSpecs/index.ts
@@ -4,17 +4,43 @@ import {
     FileHtmlAttr,
     transform as fileTransform,
 } from '@diplodoc/file-extension';
+import type {Node} from 'prosemirror-model';
 
 import type {Extension} from '../../../../core';
 import {nodeTypeFactory} from '../../../../utils/schema';
 
-import {KNOWN_ATTRS, LINK_TO_FILE_ATTRS_MAP, fileNodeAttrsSpec, yfmFileNodeName} from './const';
+import {
+    KNOWN_ATTRS,
+    LINK_TO_FILE_ATTRS_MAP,
+    YFM_FILE_DIRECTIVE_ATTRS,
+    YfmFileAttr,
+    fileNodeAttrsSpec,
+    yfmFileNodeName,
+} from './const';
 
-export {yfmFileNodeName} from './const';
+export {yfmFileNodeName, YfmFileAttr} from './const';
 export const fileType = nodeTypeFactory(yfmFileNodeName);
 
+declare global {
+    namespace MarkdownEditor {
+        interface DirectiveSyntaxAdditionalSupportedExtensions {
+            // Mark in global types that YfmFile has support for directive syntax
+            yfmFile: true;
+        }
+    }
+}
+
 export const YfmFileSpecs: Extension = (builder) => {
-    builder.configureMd((md) => md.use(fileTransform({bundle: false})));
+    const directiveContext = builder.context.get('directiveSyntax');
+
+    builder.configureMd((md) =>
+        md.use(
+            fileTransform({
+                bundle: false,
+                directiveSyntax: directiveContext?.mdPluginValueFor('yfmFile'),
+            }),
+        ),
+    );
     builder.addNode(yfmFileNodeName, () => ({
         spec: {
             group: 'inline',
@@ -57,14 +83,26 @@ export const YfmFileSpecs: Extension = (builder) => {
                 name: yfmFileNodeName,
                 type: 'node',
                 getAttrs: (tok) => {
-                    return Object.fromEntries(tok.attrs ?? []);
+                    const attrs = Object.fromEntries(tok.attrs || []);
+                    attrs[YfmFileAttr.Markup] = tok.markup;
+                    return attrs;
                 },
             },
         },
         toMd: (state, node) => {
+            if (
+                directiveContext?.shouldSerializeToDirective(
+                    'yfmFile',
+                    node.attrs[YfmFileAttr.Markup],
+                )
+            ) {
+                state.write(serializeToDirective(node));
+                return;
+            }
+
             const attrsStr = Object.entries(node.attrs)
                 .reduce<string[]>((arr, [key, value]) => {
-                    if (value) {
+                    if (key !== YfmFileAttr.Markup && value) {
                         if (key in LINK_TO_FILE_ATTRS_MAP) {
                             key = LINK_TO_FILE_ATTRS_MAP[key];
                         }
@@ -78,3 +116,18 @@ export const YfmFileSpecs: Extension = (builder) => {
         },
     }));
 };
+
+function serializeToDirective(node: Node): string {
+    const filename: string = node.attrs[YfmFileAttr.Name] || '';
+    const filelink: string = node.attrs[YfmFileAttr.Link] || '';
+
+    let fileMarkup = `:file[${filename}](${filelink})`;
+    const attrs = YFM_FILE_DIRECTIVE_ATTRS.reduce<string[]>((acc, key) => {
+        const value = node.attrs[key];
+        if (value) acc.push(`${key}="${value}"`);
+        return acc;
+    }, []);
+    if (attrs.length) fileMarkup += `{${attrs.join(' ')}}`;
+
+    return fileMarkup;
+}
