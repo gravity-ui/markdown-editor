a7c23b59af7f2d7a8fd52e3cdb927468854f6c09
Yuriy Demidov <d3m1d0v@yandex-team.ru>
2024-12-17 19:29:59 +0300

    fix(Checkbox): added parse dom rules and fixed pasting of checkboxes (#523)

--- src/extensions/yfm/Checkbox/CheckboxSpecs/schema.ts
+++ src/extensions/yfm/Checkbox/CheckboxSpecs/schema.ts
@@ -1,8 +1,8 @@
-import type {NodeSpec} from 'prosemirror-model';
+import {Fragment, type NodeSpec} from 'prosemirror-model';
 
-import {PlaceholderOptions} from '../../../../utils/placeholder';
+import type {PlaceholderOptions} from '../../../../utils/placeholder';
 
-import {CheckboxAttr, CheckboxNode, b} from './const';
+import {CheckboxAttr, CheckboxNode, b, checkboxInputType, checkboxLabelType} from './const';
 
 import type {CheckboxSpecsOptions} from './index';
 
@@ -13,14 +13,51 @@ export const getSchemaSpecs = (
     placeholder?: PlaceholderOptions,
 ): Record<CheckboxNode, NodeSpec> => ({
     [CheckboxNode.Checkbox]: {
-        group: 'block',
+        group: 'block checkbox',
         content: `${CheckboxNode.Input} ${CheckboxNode.Label}`,
         selectable: true,
         allowSelection: false,
-        parseDOM: [],
         attrs: {
             [CheckboxAttr.Class]: {default: b()},
         },
+        parseDOM: [
+            {
+                tag: 'div.checkbox',
+                priority: 100,
+                getContent(node, schema) {
+                    const input = (node as HTMLElement).querySelector<HTMLInputElement>(
+                        'input[type=checkbox]',
+                    );
+                    const label = (node as HTMLElement).querySelector<HTMLLabelElement>(
+                        'label[for]',
+                    );
+
+                    const checked = input?.checked ? 'true' : null;
+                    const text = label?.textContent;
+
+                    return Fragment.from([
+                        checkboxInputType(schema).create({[CheckboxAttr.Checked]: checked}),
+                        checkboxLabelType(schema).create(null, text ? schema.text(text) : null),
+                    ]);
+                },
+            },
+            {
+                tag: 'input[type=checkbox]',
+                priority: 50,
+                getContent(node, schema) {
+                    const id = (node as HTMLElement).id;
+                    const checked = (node as HTMLInputElement).checked ? 'true' : null;
+                    const text = node.parentNode?.querySelector<HTMLLabelElement>(
+                        `label[for=${id}]`,
+                    )?.textContent;
+
+                    return Fragment.from([
+                        checkboxInputType(schema).create({[CheckboxAttr.Checked]: checked}),
+                        checkboxLabelType(schema).create(null, text ? schema.text(text) : null),
+                    ]);
+                },
+            },
+        ],
         toDOM(node) {
             return ['div', node.attrs, 0];
         },
@@ -28,7 +65,7 @@ export const getSchemaSpecs = (
     },
 
     [CheckboxNode.Input]: {
-        group: 'block',
+        group: 'block checkbox',
         parseDOM: [],
         attrs: {
             [CheckboxAttr.Type]: {default: 'checkbox'},
@@ -45,13 +82,20 @@ export const getSchemaSpecs = (
 
     [CheckboxNode.Label]: {
         content: 'inline*',
-        group: 'block',
+        group: 'block checkbox',
         parseDOM: [
             {
                 tag: `span[class="${b('label')}"]`,
                 getAttrs: (node) => ({
                     [CheckboxAttr.For]: (node as Element).getAttribute(CheckboxAttr.For) || '',
                 }),
+            },
+            {
+                // input handled by checkbox node parse rule
+                // ignore label
+                tag: 'input[type=checkbox] ~ label[for]',
+                ignore: true,
+                consuming: true,
             },
         ],
         attrs: {
--- tests/parse-dom.ts
+++ tests/parse-dom.ts
@@ -1,11 +1,12 @@
 /* eslint-disable no-implicit-globals */
 import type {Node, Schema} from 'prosemirror-model';
-import {EditorState} from 'prosemirror-state';
+import {EditorState, type Plugin} from 'prosemirror-state';
 import {EditorView} from 'prosemirror-view';
+
 import {dispatchPasteEvent} from './dispatch-event';
 
-export function parseDOM(schema: Schema, html: string, doc: Node): void {
-    const view = new EditorView(null, {state: EditorState.create({schema})});
+export function parseDOM(schema: Schema, html: string, doc: Node, plugins?: Plugin[]): void {
+    const view = new EditorView(null, {state: EditorState.create({schema}), plugins});
     dispatchPasteEvent(view, {'text/html': html});
     expect(view.state.doc).toMatchNode(doc);
 }
