deafe20cc364474580d993801e32fee227cc8832
Sergey Makhnatkin <makhnatkin@yandex-team.ru>
2024-12-23 16:14:52 +0300

    feat(toolbars): updated flattenPreset (#531)

---
+++ src/bundle/toolbar/utils/flattenPreset.ts
@@ -0,0 +1,15 @@
+import {ToolbarDataType} from '../../../toolbar/types';
+import type {MToolbarData, MToolbarItemData, WToolbarData, WToolbarItemData} from '../../config';
+
+export const flattenPreset = <T extends WToolbarData | MToolbarData>(
+    config: T,
+): T extends WToolbarData ? WToolbarItemData[] : MToolbarItemData[] => {
+    return config.flat().reduce<(WToolbarItemData | MToolbarItemData)[]>((acc, item) => {
+        if (item.type === ToolbarDataType.ListButton && Array.isArray(item.data)) {
+            return acc.concat(item.data);
+        }
+
+        acc.push(item as WToolbarItemData | MToolbarItemData);
+        return acc;
+    }, []) as unknown as T extends WToolbarData ? WToolbarItemData[] : MToolbarItemData[];
+};
