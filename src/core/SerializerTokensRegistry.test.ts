d84ba73a64494d00ce4f902fd8b8e4260aaf914f
makhnatkin <makhnatkin@yandex-team.ru>
2025-01-22 10:25:48 +0100

    feat(core): fixed tests

--- src/core/SerializerTokensRegistry.test.ts
+++ src/core/SerializerTokensRegistry.test.ts
@@ -1,28 +1,32 @@
 import {SerializerTokensRegistry} from './SerializerTokensRegistry';
+import {MarkupManager} from './markdown/MarkupManager';
 import type {SerializerTests} from './types/serializer';
 
 describe('SerializerTokensRegistry', () => {
     it('should create empty serializer', () => {
-        const serializer = new SerializerTokensRegistry().createSerializer();
+        const markupManager = new MarkupManager();
+        const serializer = new SerializerTokensRegistry().createSerializer(markupManager);
         expect(typeof serializer.serialize).toBe('function');
     });
 
     it('should add nodes', () => {
         const nodeName = 'example_node';
+        const markupManager = new MarkupManager();
 
         const serializer = new SerializerTokensRegistry()
             .addNode(nodeName, () => {})
-            .createSerializer() as SerializerTests;
+            .createSerializer(markupManager) as SerializerTests;
 
         expect(serializer.containsNode(nodeName)).toBe(true);
     });
 
     it('should add marks', () => {
         const markName = 'example_mark';
+        const markupManager = new MarkupManager();
 
         const serializer = new SerializerTokensRegistry()
             .addMark(markName, {open: '1', close: '2'})
-            .createSerializer() as SerializerTests;
+            .createSerializer(markupManager) as SerializerTests;
 
         expect(serializer.containsMark(markName)).toBe(true);
     });
