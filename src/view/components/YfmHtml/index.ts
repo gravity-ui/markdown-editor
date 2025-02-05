7df11295964cfafcc016b9b2fe54302181899950
makhnatkin <makhnatkin@yandex-team.ru>
2025-02-05 10:27:46 +0100

    feat!: removed deprecated properties

--- src/view/components/YfmHtml/index.ts
+++ src/view/components/YfmHtml/index.ts
@@ -1,6 +1,2 @@
-import type {YfmStaticViewProps as OriginalYfmStaticViewProps} from './YfmStaticView';
-import {YfmStaticView as OriginalYfmStaticView} from './YfmStaticView';
-
-export type YFMStaticViewProps = OriginalYfmStaticViewProps;
-
-export {OriginalYfmStaticView as YfmStaticView};
+export type {YfmStaticViewProps} from './YfmStaticView';
+export {YfmStaticView} from './YfmStaticView';
