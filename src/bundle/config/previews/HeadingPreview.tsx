a2aaaddf6d94d6e7871d173c89efedfcaeeb1e9a
vladkrk <vladkrk@yandex-team.ru>
2025-02-07 14:53:12 +0300

    Fix preview headers styles

--- src/bundle/config/previews/HeadingPreview.tsx
+++ src/bundle/config/previews/HeadingPreview.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
 
+import {cn} from '../../../classname';
 import {i18n} from '../../../i18n/action-previews';
 
 import {ActionPreview} from './ActionPreview';
+
+import './ActionPreview.scss';
+
+const b = cn('action-preview');
 
 type HeadingLevel = 1 | 2 | 3 | 4 | 5 | 6;
 
 const DynamicHeading = ({level, children}: {level: HeadingLevel; children: React.ReactNode}) => {
     const HeadingTag = `h${level}`; // e.g. 'h1', 'h2', etc.
 
-    return React.createElement(HeadingTag, {}, children);
+    return React.createElement(HeadingTag, {className: b('heading', {level})}, children);
 };
 
 export const HeadingPreview = ({level}: {level: HeadingLevel}) => {
     return (
         <ActionPreview>
             <DynamicHeading level={level}>{i18n('heading')}</DynamicHeading>
-            <p>{i18n('text-with-head')}</p>
+            <p className={b('text-with-head')}>{i18n('text-with-head')}</p>
         </ActionPreview>
     );
 };
