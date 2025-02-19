b52945349472656eb0f21c6e6b3f93919126987d
Yuriy Demidov <d3m1d0v@yandex-team.ru>
2025-02-19 20:38:32 +0300

    feat(ImgSize): added ability to render custom form in image widget (#639)

--- src/extensions/yfm/ImgSize/ImageWidget/index.ts
+++ src/extensions/yfm/ImgSize/ImageWidget/index.ts
@@ -2,6 +2,7 @@ import type {Action, ExtensionAuto} from '../../../../core';
 import type {FileUploadHandler} from '../../../../utils/upload';
 
 import {addImageWidget} from './actions';
+import type {RenderImageWidgetFormFn} from './view';
 import type {ImageWidgetDescriptorOpts} from './widget';
 
 const addImageWidgetAction = 'addImageWidget';
@@ -11,12 +12,14 @@ export type ImageWidgetOptions = Pick<
     'needToSetDimensionsForUploadedImages' | 'enableNewImageSizeCalculation'
 > & {
     imageUploadHandler?: FileUploadHandler;
+    renderImageWidgetForm?: RenderImageWidgetFormFn;
 };
 
 export const ImageWidget: ExtensionAuto<ImageWidgetOptions> = (builder, opts) => {
     builder.addAction(addImageWidgetAction, (deps) =>
         addImageWidget(deps, {
             uploadImages: opts.imageUploadHandler,
+            renderImageForm: opts.renderImageWidgetForm,
             needToSetDimensionsForUploadedImages: opts.needToSetDimensionsForUploadedImages,
             enableNewImageSizeCalculation: opts.enableNewImageSizeCalculation,
         }),
