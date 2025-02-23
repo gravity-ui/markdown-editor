e44315277381bb9e099a691dff5c1193545767ee
reffat <reffat@yandex-team.ru>
2025-02-23 20:51:48 +0300

    fix(link): fixed cursor position

--- src/extensions/markdown/Link/plugins/LinkTooltipPlugin/index.tsx
+++ src/extensions/markdown/Link/plugins/LinkTooltipPlugin/index.tsx
@@ -111,14 +111,12 @@ class SelectionTooltip implements PluginView {
         if (prevState && prevState.doc.eq(state.doc) && prevState.selection.eq(state.selection))
             return;
 
-        const prevTextNode = this.textNode;
         this.textNode = getTextNode(view.state);
 
         const prevRef = this.textNodeRef;
         this.updateTextNodeRef();
 
         if (!this.textNode || !this.textNodeRef) {
-            this.removePlaceholderLink(prevTextNode);
             this.hideTooltip();
             return;
         }
@@ -235,13 +233,11 @@ class SelectionTooltip implements PluginView {
         if (normalizeResult) {
             const {url} = normalizeResult;
             const {from, to} = textNode;
-            view.dispatch(
-                view.state.tr.addMark(from, to, linkType(view.state.schema).create({href: url})),
-            );
 
-            view.dispatch(view.state.tr.setSelection(TextSelection.create(view.state.doc, to)));
-
-            setTimeout(this.cancelPopup.bind(this));
+            const tr = view.state.tr;
+            tr.setSelection(TextSelection.create(tr.doc, tr.mapping.map(to)));
+            tr.addMark(from, to, linkType(view.state.schema).create({href: url}));
+            view.dispatch(tr);
         }
     }
 
