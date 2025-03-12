b1b4e6b268df017b1929cd7fdc4664226fe15388
d3m1d0v <d3m1d0v@yandex-team.ru>
2025-03-12 18:43:19 +0300

    feat(react-utils): added helper hook for manage shared editing state

--- src/react-utils/index.ts
+++ src/react-utils/index.ts
@@ -3,4 +3,5 @@ export * from './hooks';
 export * from './react-node-view';
 export * from './useNodeEditing';
 export * from './useNodeHovered';
+export * from './useSharedEditingState';
 export * from './useSticky';
