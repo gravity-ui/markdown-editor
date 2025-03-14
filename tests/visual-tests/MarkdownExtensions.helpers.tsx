166e78c2dfa40c404009a9d7745b5958360ea177
Sergey Makhnatkin <makhnatkin@yandex-team.ru>
2025-03-14 16:29:39 +0100

    test: disabled sticky toolbar (#684)

--- tests/visual-tests/MarkdownExtensions.helpers.tsx
+++ tests/visual-tests/MarkdownExtensions.helpers.tsx
@@ -2,4 +2,10 @@ import {composeStories} from '@storybook/react';
 
 import * as DefaultMarkdownStories from '../../demo/stories/markdown/Markdown.stories';
 
-export const MarkdownStories = composeStories(DefaultMarkdownStories);
+export const MarkdownStories = composeStories(DefaultMarkdownStories, {
+    argsEnhancers: [
+        () => ({
+            stickyToolbar: false,
+        }),
+    ],
+});
