085cb3bd13ffac2855f5e03dffbf31008334da2a
saxumcordis <victor123952@gmail.com>
2025-04-21 15:16:31 +0300

    feat: add hidden_commit to dynamic modifiers

--- src/bundle/config/dynamicModifiers.ts
+++ src/bundle/config/dynamicModifiers.ts
@@ -4,8 +4,8 @@ import {v5} from 'uuid';
 import type {DynamicModifiers} from '../../core/types/dynamicModifiers';
 import type {MarkupManager} from '../MarkupManager';
 
-const YFM_TABLE_TOKEN_ATTR = 'data-token-id';
-const YFM_TABLE_NODE_ATTR = 'data-node-id';
+const YFM_TOKEN_ATTR = 'data-token-id';
+const YFM_NODE_ATTR = 'data-node-id';
 
 export function createDynamicModifiers(markupManager: MarkupManager): DynamicModifiers[] {
     return [
@@ -24,10 +24,22 @@ export function createDynamicModifiers(markupManager: MarkupManager): DynamicMod
                     const tokenId = v5(content, markupManager.getNamespace());
 
                     if (/^\s*#\|/.test(content)) {
-                        token.attrSet(YFM_TABLE_TOKEN_ATTR, tokenId);
+                        token.attrSet(YFM_TOKEN_ATTR, tokenId);
                         markupManager.setMarkup(tokenId, dedent(content));
                     }
                 }
+                return token;
+            },
+        },
+        {
+            type: 'parserToken',
+            tokenName: 'hidden_comment',
+            process: (token, _, rawMarkup) => {
+                const tokenId = v5(rawMarkup, markupManager.getNamespace());
+
+                token.attrSet(YFM_TOKEN_ATTR, tokenId);
+                markupManager.setMarkup(tokenId, dedent(rawMarkup));
+
                 return token;
             },
         },
@@ -39,14 +51,36 @@ export function createDynamicModifiers(markupManager: MarkupManager): DynamicMod
              */
             process: (token, attrs) => ({
                 ...attrs,
-                [YFM_TABLE_NODE_ATTR]: token.attrGet(YFM_TABLE_TOKEN_ATTR),
+                [YFM_NODE_ATTR]: token.attrGet(YFM_TOKEN_ATTR),
+            }),
+        },
+        {
+            type: 'parserNodeAttrs',
+            tokenName: 'hidden_comment',
+            /**
+             * - Links the token to its corresponding node via `data-node-id`.
+             */
+            process: (token, attrs) => ({
+                ...attrs,
+                [YFM_NODE_ATTR]: token.attrGet(YFM_TOKEN_ATTR),
             }),
         },
         {
             type: 'parserNode',
             nodeName: 'yfm_table',
             process: (node) => {
-                const nodeId = node.attrs[YFM_TABLE_NODE_ATTR];
+                const nodeId = node.attrs[YFM_NODE_ATTR];
+                if (nodeId) {
+                    markupManager.setNode(nodeId, node);
+                }
+                return node;
+            },
+        },
+        {
+            type: 'parserNode',
+            nodeName: 'hidden_comment',
+            process: (node) => {
+                const nodeId = node.attrs[YFM_NODE_ATTR];
                 if (nodeId) {
                     markupManager.setNode(nodeId, node);
                 }
@@ -62,7 +96,7 @@ export function createDynamicModifiers(markupManager: MarkupManager): DynamicMod
              * - Falls back to schema-based rendering if the node structure, attributes, or parent elements affect it.
              */
             process: (state, node, parent, index, callback) => {
-                const nodeId = node.attrs[YFM_TABLE_NODE_ATTR];
+                const nodeId = node.attrs[YFM_NODE_ATTR];
                 const savedNode = markupManager.getNode(nodeId);
 
                 if (savedNode?.eq(node)) {
@@ -79,12 +113,43 @@ export function createDynamicModifiers(markupManager: MarkupManager): DynamicMod
             },
         },
         {
+            type: 'serializerNode',
+            nodeName: 'hidden_comment',
+            /**
+             * - Retrieves the original Markdown using the `data-node-id` attribute.
+             * - Uses the original Markdown if the node matches the saved version.
+             * - Falls back to schema-based rendering if the node structure, attributes, or parent elements affect it.
+             */
+            process: (state, node, parent, index, callback) => {
+                const nodeId = node.attrs[YFM_NODE_ATTR];
+                const savedNode = markupManager.getNode(nodeId);
+
+                if (savedNode?.eq(node)) {
+                    const content: string = markupManager.getMarkup(nodeId) || '';
+                    state.ensureNewLine();
+                    state.text(content, false);
+                    state.ensureNewLine();
+                    return;
+                }
+
+                callback?.(state, node, parent, index);
+            },
+        },
+        {
             type: 'schemaNodeSpec',
             nodeName: 'yfm_table',
             /**
              * - Adds the `data-node-id` attribute to the list of allowed attributes.
              */
-            allowedAttrs: [YFM_TABLE_NODE_ATTR],
+            allowedAttrs: [YFM_NODE_ATTR],
+        },
+        {
+            type: 'schemaNodeSpec',
+            nodeName: 'hidden_comment',
+            /**
+             * - Adds the `data-node-id` attribute to the list of allowed attributes.
+             */
+            allowedAttrs: [YFM_NODE_ATTR],
         },
     ];
 }
