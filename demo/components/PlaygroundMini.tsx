a91ec2ad724fd6e4b1089740306830990a65bf0c
dandmechenko <dandmechenko@yandex-team.ru>
2025-04-24 13:50:46 +0200

    feat(Markdown): DisableSearchPanel prop

--- demo/components/PlaygroundMini.tsx
+++ demo/components/PlaygroundMini.tsx
@@ -15,6 +15,7 @@ export type PlaygroundMiniProps = Pick<
     | 'sanitizeHtml'
     | 'prepareRawMarkup'
     | 'splitModeOrientation'
+    | 'searchPanel'
     | 'stickyToolbar'
     | 'initialSplitModeEnabled'
     | 'renderPreviewDefined'
