a070d2a70279c85f8e4afc47731d16a92d9536de
d3m1d0v <d3m1d0v@yandex-team.ru>
2025-05-28 16:50:42 +0300

    fix(Bold): preserve bold markup

--- src/extensions/markdown/Bold/BoldSpecs/index.ts
+++ src/extensions/markdown/Bold/BoldSpecs/index.ts
@@ -1,30 +1,49 @@
-import type {ExtensionAuto} from '../../../../core';
-import {markTypeFactory} from '../../../../utils/schema';
+import type {ExtensionAuto} from '#core';
+import type {Mark} from '#pm/model';
+import {markTypeFactory} from 'src/utils/schema';
 
 export const boldMarkName = 'strong';
 export const boldType = markTypeFactory(boldMarkName);
+export const BoldAttrs = {
+    Markup: 'data-markup',
+} as const;
+const defaultMarkup = '**';
 
 export const BoldSpecs: ExtensionAuto = (builder) => {
     builder.addMark(boldMarkName, () => ({
         spec: {
+            attrs: {[BoldAttrs.Markup]: {default: defaultMarkup}},
             parseDOM: [
                 {tag: 'b'},
-                {tag: 'strong'},
+                {
+                    tag: 'strong',
+                    getAttrs: (node) => ({
+                        [BoldAttrs.Markup]: node.getAttribute(BoldAttrs.Markup),
+                    }),
+                },
                 {
                     style: 'font-weight',
                     getAttrs: (value) => /^(bold(er)?|[5-9]\d{2,})$/.test(value as string) && null,
                 },
             ],
-            toDOM() {
-                return ['strong'];
+            toDOM(mark) {
+                return ['strong', mark.attrs];
             },
         },
         fromMd: {
             tokenSpec: {
                 name: boldMarkName,
                 type: 'mark',
+                getAttrs: (token) => ({
+                    [BoldAttrs.Markup]: token.markup,
+                }),
             },
         },
-        toMd: {open: '**', close: '**', mixable: true, expelEnclosingWhitespace: true},
+        toMd: {open: getMarkup, close: getMarkup, mixable: true, expelEnclosingWhitespace: true},
     }));
 };
+
+function getMarkup(_: unknown, mark: Mark): string {
+    const attr = mark.attrs[BoldAttrs.Markup];
+    return attr || defaultMarkup;
+}
