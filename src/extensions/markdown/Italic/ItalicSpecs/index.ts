ef69f892035b1b6f2d2db8753317ab9f185459a0
Yuriy Demidov <d3m1d0v@yandex-team.ru>
2025-05-29 14:59:45 +0300

    fix(wysiwyg): preserve markup for bold and italic marks (#774)

--- src/extensions/markdown/Italic/ItalicSpecs/index.ts
+++ src/extensions/markdown/Italic/ItalicSpecs/index.ts
@@ -1,22 +1,46 @@
-import type {ExtensionAuto} from '../../../../core';
-import {markTypeFactory} from '../../../../utils/schema';
+import type {ExtensionAuto} from '#core';
+import type {Mark} from '#pm/model';
+import {markTypeFactory} from 'src/utils/schema';
 
 export const italicMarkName = 'em';
 export const italicType = markTypeFactory(italicMarkName);
+export const ItalicAttrs = {
+    Markup: 'data-markup',
+} as const;
+const defaultMarkup = '*';
 
 export const ItalicSpecs: ExtensionAuto = (builder) => {
     builder.addMark(italicMarkName, () => ({
         spec: {
+            attrs: {[ItalicAttrs.Markup]: {default: defaultMarkup}},
             parseDOM: [
                 {tag: 'i'},
-                {tag: 'em'},
+                {
+                    tag: 'em',
+                    getAttrs: (node) => ({
+                        [ItalicAttrs.Markup]: node.getAttribute(ItalicAttrs.Markup),
+                    }),
+                },
                 {style: 'font-style', getAttrs: (value) => value === 'italic' && null},
             ],
-            toDOM() {
-                return ['em'];
+            toDOM(mark) {
+                return ['em', mark.attrs];
             },
         },
-        toMd: {open: '*', close: '*', mixable: true, expelEnclosingWhitespace: true},
-        fromMd: {tokenSpec: {name: italicMarkName, type: 'mark'}},
+        toMd: {open: getMarkup, close: getMarkup, mixable: true, expelEnclosingWhitespace: true},
+        fromMd: {
+            tokenSpec: {
+                name: italicMarkName,
+                type: 'mark',
+                getAttrs: (token) => ({
+                    [ItalicAttrs.Markup]: token.markup,
+                }),
+            },
+        },
     }));
 };
+
+function getMarkup(_: unknown, mark: Mark): string {
+    const attr = mark.attrs[ItalicAttrs.Markup];
+    return attr || defaultMarkup;
+}
