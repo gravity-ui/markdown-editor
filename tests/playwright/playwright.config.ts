87d5c8dd8fef33cb54e699ff819a56d225b23514
makhnatkin <makhnatkin@yandex-team.ru>
2025-07-08 16:34:42 +0200

    chore: increase Playwright workers to 4

--- tests/playwright/playwright.config.ts
+++ tests/playwright/playwright.config.ts
@@ -57,7 +57,7 @@ const config: PlaywrightTestConfig = {
     /* Retry on CI only */
     retries: process.env.CI ? 2 : 0,
     /* Opt out of parallel tests on CI. */
-    workers: process.env.CI ? 2 : 2,
+    workers: process.env.CI ? 4 : 4,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: [
         ['list'],
