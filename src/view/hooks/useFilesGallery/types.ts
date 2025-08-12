1c7f49e8a15880d1cc8f241ee5ba4254369a54b0
kseniyakuzina <kseniyakuzina@yandex-team.ru>
2025-08-12 09:39:53 +0300

    feat(useFilesGallery): add possibility to override gallery item props

--- src/view/hooks/useFilesGallery/types.ts
+++ src/view/hooks/useFilesGallery/types.ts
@@ -4,3 +4,16 @@ export type GalleryItemPropsWithUrl = GalleryItemProps & {
     // pass the url to be able to exclude the items from content if they are found in the custom files
     url?: string;
 };
+
+export type FilesGalleryItemType = 'image' | 'video';
+
+export type UseFilesGalleryOptions = {
+    download?: (url: string, type: FilesGalleryItemType, element: Element) => string | undefined;
+    copyUrl?: (url: string, type: FilesGalleryItemType, element: Element) => string | undefined;
+    overrideItemProps?: (
+        url: string,
+        type: FilesGalleryItemType,
+        element: Element,
+        currentProps: GalleryItemProps,
+    ) => GalleryItemProps;
+};
