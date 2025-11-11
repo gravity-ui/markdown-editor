d0ebf2ce9e679b765446a1481475e5822acc417b
d3m1d0v <d3m1d0v@yandex-team.ru>
2025-11-11 18:44:12 +0300

    feat(Deflist): add data-line attribute to term

--- src/extensions/markdown/Deflist/DeflistSpecs/parser.ts
+++ src/extensions/markdown/Deflist/DeflistSpecs/parser.ts
@@ -1,11 +1,19 @@
 import type {ParserToken} from '../../../../core';
 
-import {DeflistNode} from './const';
+import {DeflistAttr, DeflistNode} from './const';
 
 export const parserTokens: Record<DeflistNode, ParserToken> = {
     [DeflistNode.List]: {name: DeflistNode.List, type: 'block'},
 
-    [DeflistNode.Term]: {name: DeflistNode.Term, type: 'block'},
+    [DeflistNode.Term]: {
+        name: DeflistNode.Term,
+        type: 'block',
+        getAttrs(token) {
+            return {
+                [DeflistAttr.Line]: token.attrGet('data-line'),
+            };
+        },
+    },
 
     [DeflistNode.Desc]: {name: DeflistNode.Desc, type: 'block'},
 };
