8e4c2f6304c890e21c2a271e1261d9cae1ccb058
Yuriy Demidov <d3m1d0v@yandex-team.ru>
2025-12-29 14:21:38 +0300

    dev: migrate to pnpm (#900)

--- tests/visual-tests/MarkdownExtensions.helpers.tsx
+++ tests/visual-tests/MarkdownExtensions.helpers.tsx
@@ -2,7 +2,9 @@ import {composeStories} from '@storybook/react';
 
 import * as DefaultMarkdownStories from '../../demo/stories/markdown/Markdown.stories';
 
-export const MarkdownStories = composeStories(DefaultMarkdownStories, {
+type Stories = ReturnType<typeof composeStories<typeof DefaultMarkdownStories>>;
+
+export const MarkdownStories: Stories = composeStories(DefaultMarkdownStories, {
     argsEnhancers: [
         () => ({
             stickyToolbar: false,
