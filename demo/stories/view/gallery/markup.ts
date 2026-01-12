863c04dd3084c26427e018a2a8fe266662fbe18b
Yuriy Demidov <d3m1d0v@yandex-team.ru>
2026-01-12 15:37:04 +0300

    feat(view): add support of yfm-files to useFilesGallery() hook

--- demo/stories/view/gallery/markup.ts
+++ demo/stories/view/gallery/markup.ts
@@ -6,5 +6,13 @@ export const galleryMarkup = `
 
 ![Dojocat](https://octodex.github.com/images/dojocat.jpg "The Dojocat")
 
+## Video
+
 [Video](https://static.videezy.com/system/resources/previews/000/013/444/original/Water_43_-_37s_-_4k_res.mp4)
+
+## Files
+
+{% file src="https://octodex.github.com/images/forktocat.jpg" name="Forktocat" type="image/jpeg" %} 
+
+{% file src="https://static.videezy.com/system/resources/previews/000/012/253/original/Water_17_-_9s_-_4k_res.mp4" name="Rings of water" type="video/mp4" %} 
 `.trim();
