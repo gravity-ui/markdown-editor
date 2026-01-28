b641ae8fa91e2761eafe3bee36a53922627f45ad
Yuriy Demidov <d3m1d0v@yandex-team.ru>
2026-01-28 18:07:19 +0300

    feat(CodeBlock): create new code blocks without line numbers (#940)

--- src/extensions/markdown/CodeBlock/const.ts
+++ src/extensions/markdown/CodeBlock/const.ts
@@ -1,5 +1,11 @@
-import {codeBlockType} from './CodeBlockSpecs';
+import {type LineNumbersOptions, codeBlockType} from './CodeBlockSpecs';
 
 export {codeBlockNodeName, CodeBlockNodeAttr as CodeBlockAttr} from './CodeBlockSpecs';
 export const cbAction = 'toCodeBlock';
 export {codeBlockType};
+
+// After changing the default, don't forget to update LineNumbersOptions
+export const lineNumbersOptionsDefault: LineNumbersOptions = {
+    enabled: true,
+    showByDefault: false,
+};
