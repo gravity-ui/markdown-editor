579940f440a5d7c54cc6dc0d15f32ec7ebb2b23c
Yuriy Demidov <d3m1d0v@yandex-team.ru>
2026-03-26 12:59:59 +0300

    fix(YfmNote): fix yfm-note tooltip view and active state

--- packages/editor/src/extensions/yfm/YfmNote/plugins/YfmNoteTooltipPlugin/YfmNoteToolbar.tsx
+++ packages/editor/src/extensions/yfm/YfmNote/plugins/YfmNoteTooltipPlugin/YfmNoteToolbar.tsx
@@ -9,7 +9,10 @@ import type {EditorView} from '#pm/view';
 import {i18n} from 'src/i18n/yfm-note';
 import {typedMemo} from 'src/react-utils/memo';
 import {Toolbar, type ToolbarData, ToolbarDataType} from 'src/toolbar';
+import {ToolbarWrapToContext} from 'src/toolbar/ToolbarRerender';
 import {removeNode} from 'src/utils/remove-node';
+
+import {NoteAttrs} from '../../const';
 
 import './YfmNoteToolbar.scss';
 
@@ -23,7 +26,6 @@ enum YfmNoteType {
 }
 
 const isEnable = () => true;
-const isActive = () => false;
 
 const changeType: (
     type: YfmNoteType,
@@ -38,8 +40,8 @@ const changeType: (
         dispatch(
             tr.setNodeMarkup(pos, null, {
                 ...node.attrs,
-                class: `yfm-note yfm-accent-${type}`,
-                'note-type': type,
+                [NoteAttrs.Class]: `yfm-note yfm-accent-${type}`,
+                [NoteAttrs.Type]: type,
             }),
         );
     };
@@ -66,7 +68,7 @@ export function YfmNoteToolbar({pos, node, editorView}: YfmNoteToolbarProps) {
                     icon: {data: CircleInfo},
                     title: i18n(type),
                     type: ToolbarDataType.SingleButton,
-                    isActive,
+                    isActive: () => nodeRef.current.attrs[NoteAttrs.Type] === type,
                     isEnable,
                     exec: (view) =>
                         changeType(type)({
@@ -83,7 +85,8 @@ export function YfmNoteToolbar({pos, node, editorView}: YfmNoteToolbarProps) {
                     icon: {data: TrashBin},
                     title: i18n('remove'),
                     type: ToolbarDataType.SingleButton,
-                    isActive,
+                    theme: 'danger',
+                    isActive: () => false,
                     isEnable,
                     exec: (view) =>
                         removeNode({
@@ -98,14 +101,16 @@ export function YfmNoteToolbar({pos, node, editorView}: YfmNoteToolbarProps) {
     }, [nodeRef, posRef]);
 
     return (
-        <ToolbarMemoized
-            editor={editorView}
-            focus={onFocus}
-            // the yfm class allows to access css variables
-            // https://github.com/diplodoc-platform/transform/blob/master/src/scss/_common.scss#L17
-            className="yfm g-md-yfm-note-toolbar"
-            qa="g-md-toolbar-yfm-note"
-            data={toolbarData}
-        />
+        <ToolbarWrapToContext editor={editorView}>
+            <ToolbarMemoized
+                editor={editorView}
+                focus={onFocus}
+                // the yfm class allows to access css variables
+                // https://github.com/diplodoc-platform/transform/blob/master/src/scss/_common.scss#L17
+                className="yfm g-md-yfm-note-toolbar"
+                qa="g-md-toolbar-yfm-note"
+                data={toolbarData}
+            />
+        </ToolbarWrapToContext>
     );
 }
