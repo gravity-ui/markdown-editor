ba24b3bdc6b0438642522684a752608171467aab
Daniil Gaponov <dagaponov@yandex-team.ru>
2026-07-26 17:23:51 +0300

    fix: lint

--- packages/editor/gulpfile.mjs
+++ packages/editor/gulpfile.mjs
@@ -1,6 +1,6 @@
+import {copyFileSync, rmSync} from 'node:fs';
 import {dirname, resolve} from 'node:path';
 import {fileURLToPath} from 'node:url';
-import {copyFileSync, rmSync} from 'node:fs';
 
 import {buildDocs} from '@gravity-ui/readme-validator';
 import {series, task} from '@markdown-editor/gulp-tasks';
