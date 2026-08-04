526282e98918bb7dc17500363ef56f1cd974f66b
Yuriy Demidov <d3m1d0v@yandex-team.ru>
2026-08-04 14:56:55 +0300

    refactor(Deflist): use new builder methods to register nodes (#1196)

--- packages/editor/src/extensions/markdown/Deflist/DeflistSpecs/index.ts
+++ packages/editor/src/extensions/markdown/Deflist/DeflistSpecs/index.ts
@@ -1,13 +1,10 @@
-import deflistPlugin from '@diplodoc/transform/lib/plugins/deflist.js';
-
 import type {ExtensionAuto} from '../../../../core';
 import {nodeTypeFactory} from '../../../../utils/schema';
 
 import {DeflistNode} from './const';
-import {parserTokens} from './parser';
-import {getSchemaSpecs} from './schema';
-import type {DeflistSchemaOptions} from './schema';
-import {serializerTokens} from './serializer';
+import {DeflistParserSpecs} from './parser';
+import {type DeflistSchemaOptions, DeflistSchemaSpecs} from './schema';
+import {DeflistSerializerSpecs} from './serializer';
 
 export {DeflistNode} from './const';
 export const defListType = nodeTypeFactory(DeflistNode.List);
@@ -19,23 +16,5 @@ export type {DeflistSchemaOptions} from './schema';
 export type DeflistSpecsOptions = DeflistSchemaOptions & {};
 
 export const DeflistSpecs: ExtensionAuto<DeflistSpecsOptions> = (builder, opts) => {
-    const schemaSpecs = getSchemaSpecs(opts, builder.context.get('placeholder'));
-
-    builder.configureMd((md) => md.use(deflistPlugin));
-    builder
-        .addNode(DeflistNode.List, () => ({
-            spec: schemaSpecs[DeflistNode.List],
-            fromMd: {tokenSpec: parserTokens[DeflistNode.List]},
-            toMd: serializerTokens[DeflistNode.List],
-        }))
-        .addNode(DeflistNode.Term, () => ({
-            spec: schemaSpecs[DeflistNode.Term],
-            fromMd: {tokenSpec: parserTokens[DeflistNode.Term]},
-            toMd: serializerTokens[DeflistNode.Term],
-        }))
-        .addNode(DeflistNode.Desc, () => ({
-            spec: schemaSpecs[DeflistNode.Desc],
-            fromMd: {tokenSpec: parserTokens[DeflistNode.Desc]},
-            toMd: serializerTokens[DeflistNode.Desc],
-        }));
+    builder.use(DeflistSchemaSpecs, opts).use(DeflistParserSpecs).use(DeflistSerializerSpecs);
 };
