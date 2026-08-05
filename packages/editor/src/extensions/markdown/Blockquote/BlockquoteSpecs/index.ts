14c1355eca197ab1029a83e5b587860ba693bad7
Yuriy Demidov <d3m1d0v@yandex-team.ru>
2026-08-05 17:09:04 +0300

    refactor(Blockquote): use new builder methods to register nodes (#1199)

--- packages/editor/src/extensions/markdown/Blockquote/BlockquoteSpecs/index.ts
+++ packages/editor/src/extensions/markdown/Blockquote/BlockquoteSpecs/index.ts
@@ -1,15 +1,15 @@
 import type {Node} from 'prosemirror-model';
 
-import type {ExtensionAuto} from '../../../../core';
-import {nodeTypeFactory} from '../../../../utils/schema';
+import type {ExtensionAuto} from '#core';
+import {nodeTypeFactory} from 'src/utils/schema';
 
 export const blockquoteNodeName = 'blockquote';
 export const blockquoteType = nodeTypeFactory(blockquoteNodeName);
 export const isBlockqouteNode = (node: Node) => node.type.name === blockquoteNodeName;
 
 export const BlockquoteSpecs: ExtensionAuto = (builder) => {
-    builder.addNode(blockquoteNodeName, () => ({
-        spec: {
+    builder
+        .addNodeSpec(blockquoteNodeName, () => ({
             content: 'block+',
             group: 'block',
             defining: true,
@@ -19,10 +19,12 @@ export const BlockquoteSpecs: ExtensionAuto = (builder) => {
             },
             selectable: true,
             selectAll: 'node',
-        },
-        fromMd: {tokenSpec: {name: blockquoteNodeName, type: 'block'}},
-        toMd: (state, node) => {
+        }))
+        .addMarkdownTokenParserSpec(blockquoteNodeName, () => ({
+            name: blockquoteNodeName,
+            type: 'block',
+        }))
+        .addNodeSerializerSpec(blockquoteNodeName, () => (state, node) => {
             state.wrapBlock('> ', null, node, () => state.renderContent(node));
-        },
-    }));
+        });
 };
