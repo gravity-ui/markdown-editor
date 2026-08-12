000ccd30e01ec9e4b16c95d3285aa6ad71d741e4
Yuriy Demidov <d3m1d0v@yandex-team.ru>
2026-08-12 13:14:54 +0300

    refactor(Underline): use new builder methods to register marks

--- packages/editor/src/extensions/markdown/Underline/UnderlineSpecs/index.ts
+++ packages/editor/src/extensions/markdown/Underline/UnderlineSpecs/index.ts
@@ -1,7 +1,7 @@
 import insPlugin from 'markdown-it-ins';
 
-import type {ExtensionAuto} from '../../../../core';
-import {markTypeFactory} from '../../../../utils/schema';
+import type {ExtensionAuto} from '#core';
+import {markTypeFactory} from 'src/utils/schema';
 
 export const underlineMarkName = 'ins';
 export const underlineType = markTypeFactory(underlineMarkName);
@@ -9,14 +9,20 @@ export const underlineType = markTypeFactory(underlineMarkName);
 export const UnderlineSpecs: ExtensionAuto = (builder) => {
     builder
         .configureMd((md) => md.use(insPlugin))
-        .addMark(underlineMarkName, () => ({
-            spec: {
-                parseDOM: [{tag: 'ins'}, {tag: 'u'}],
-                toDOM() {
-                    return ['ins'];
-                },
+        .addMarkSpec(underlineMarkName, () => ({
+            parseDOM: [{tag: 'ins'}, {tag: 'u'}],
+            toDOM() {
+                return ['ins'];
             },
-            toMd: {open: '++', close: '++', mixable: true, expelEnclosingWhitespace: true},
-            fromMd: {tokenSpec: {name: underlineMarkName, type: 'mark'}},
+        }))
+        .addMarkdownTokenParserSpec('ins', () => ({
+            name: underlineMarkName,
+            type: 'mark',
+        }))
+        .addMarkSerializerSpec(underlineMarkName, () => ({
+            open: '++',
+            close: '++',
+            mixable: true,
+            expelEnclosingWhitespace: true,
         }));
 };
