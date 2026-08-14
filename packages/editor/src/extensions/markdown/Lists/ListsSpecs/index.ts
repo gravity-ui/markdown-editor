7cb4013809816af586e490827ecb18fd38396fbf
Yuriy Demidov <d3m1d0v@yandex-team.ru>
2026-08-14 15:45:44 +0300

    refactor(Lists): use new builder methods to register nodes (#1213)

--- packages/editor/src/extensions/markdown/Lists/ListsSpecs/index.ts
+++ packages/editor/src/extensions/markdown/Lists/ListsSpecs/index.ts
@@ -1,10 +1,10 @@
-import type {ExtensionAuto} from '../../../../core';
-import {nodeTypeFactory} from '../../../../utils/schema';
+import type {ExtensionAuto} from '#core';
+import {nodeTypeFactory} from 'src/utils/schema';
 
 import {ListNode} from './const';
-import {parserTokens} from './parser';
-import {schemaSpecs} from './schema';
-import {serializerTokens} from './serializer';
+import {ListsParserSpecs} from './parser';
+import {ListsSchemaSpecs} from './schema';
+import {ListsSerializerSpecs} from './serializer';
 
 export {ListsAttr, ListNode} from './const';
 export const liType = nodeTypeFactory(ListNode.ListItem);
@@ -12,20 +12,5 @@ export const blType = nodeTypeFactory(ListNode.BulletList);
 export const olType = nodeTypeFactory(ListNode.OrderedList);
 
 export const ListsSpecs: ExtensionAuto = (builder) => {
-    builder
-        .addNode(ListNode.ListItem, () => ({
-            spec: schemaSpecs[ListNode.ListItem],
-            toMd: serializerTokens[ListNode.ListItem],
-            fromMd: {tokenSpec: parserTokens[ListNode.ListItem]},
-        }))
-        .addNode(ListNode.BulletList, () => ({
-            spec: schemaSpecs[ListNode.BulletList],
-            toMd: serializerTokens[ListNode.BulletList],
-            fromMd: {tokenSpec: parserTokens[ListNode.BulletList]},
-        }))
-        .addNode(ListNode.OrderedList, () => ({
-            spec: schemaSpecs[ListNode.OrderedList],
-            toMd: serializerTokens[ListNode.OrderedList],
-            fromMd: {tokenSpec: parserTokens[ListNode.OrderedList]},
-        }));
+    builder.use(ListsSchemaSpecs).use(ListsParserSpecs).use(ListsSerializerSpecs);
 };
