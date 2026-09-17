81a47d329ae2099634ea2a5c33b72559b22e3313
Yuriy Demidov <d3m1d0v@yandex-team.ru>
2026-09-17 13:58:58 +0300

    refactor(Mermaid): use new builder methods to register nodes (#1246)

--- packages/editor/src/extensions/additional/Mermaid/MermaidSpecs/index.tsx
+++ packages/editor/src/extensions/additional/Mermaid/MermaidSpecs/index.tsx
@@ -7,51 +7,51 @@ import {MermaidConsts, defaultMermaidEntityId, mermaidNodeName} from './const';
 export {mermaidNodeName, MermaidConsts} from './const';
 
 export type MermaidSpecsOptions = {
+    /** @deprecated Register the view with builder.addNodeView() after the specs. */
     nodeView?: ExtensionNodeSpec['view'];
 };
 
 const MermaidSpecsExtension: ExtensionAuto<MermaidSpecsOptions> = (builder, {nodeView}) => {
     builder
         .configureMd((md) => md.use(transform({runtime: 'mermaid', bundle: false}), {}))
-        .addNode(mermaidNodeName, () => ({
-            fromMd: {
-                tokenSpec: {
-                    name: mermaidNodeName,
-                    type: 'node',
-                    getAttrs: ({content}) => ({
-                        [MermaidConsts.NodeAttrs.content]: content,
-                        [MermaidConsts.NodeAttrs.EntityId]: generateEntityId(mermaidNodeName),
-                    }),
-                },
+        .addNodeSpec(mermaidNodeName, () => ({
+            selectable: true,
+            atom: true,
+            group: 'block',
+            attrs: {
+                [MermaidConsts.NodeAttrs.content]: {default: ''},
+                [MermaidConsts.NodeAttrs.class]: {default: 'mermaid'},
+                [MermaidConsts.NodeAttrs.EntityId]: {default: defaultMermaidEntityId},
+                [MermaidConsts.NodeAttrs.newCreated]: {default: null},
             },
-            spec: {
-                selectable: true,
-                atom: true,
-                group: 'block',
-                attrs: {
-                    [MermaidConsts.NodeAttrs.content]: {default: ''},
-                    [MermaidConsts.NodeAttrs.class]: {default: 'mermaid'},
-                    [MermaidConsts.NodeAttrs.EntityId]: {default: defaultMermaidEntityId},
-                    [MermaidConsts.NodeAttrs.newCreated]: {default: null},
-                },
-                parseDOM: [],
-                toDOM(node) {
-                    return ['div', node.attrs];
-                },
-                dnd: {props: {offset: [8, 1]}},
+            parseDOM: [],
+            toDOM(node) {
+                return ['div', node.attrs];
             },
-            toMd: (state, node) => {
-                const content: string = node.attrs[MermaidConsts.NodeAttrs.content] || '';
+            dnd: {props: {offset: [8, 1]}},
+        }))
+        .addMarkdownTokenParserSpec('mermaid', () => ({
+            name: mermaidNodeName,
+            type: 'node',
+            getAttrs: ({content}) => ({
+                [MermaidConsts.NodeAttrs.content]: content,
+                [MermaidConsts.NodeAttrs.EntityId]: generateEntityId(mermaidNodeName),
+            }),
+        }))
+        .addNodeSerializerSpec(mermaidNodeName, () => (state, node) => {
+            const content: string = node.attrs[MermaidConsts.NodeAttrs.content] || '';
 
-                state.write('```mermaid');
-                state.ensureNewLine();
-                state.text(content.trim(), false);
-                state.ensureNewLine();
-                state.write('```');
-                state.ensureNewLine();
-            },
-            view: nodeView,
-        }));
+            state.write('```mermaid');
+            state.ensureNewLine();
+            state.text(content.trim(), false);
+            state.ensureNewLine();
+            state.write('```');
+            state.ensureNewLine();
+        });
+
+    if (nodeView) {
+        builder.addNodeView(mermaidNodeName, nodeView);
+    }
 };
 
 export const MermaidSpecs = Object.assign(MermaidSpecsExtension, MermaidConsts);
